9665afc717201591417890321477d7f67e7b86a3
Zaenal Arifin <zaenal.ariefin24@gmail.com>
2025-08-05 15:59:38 +0700

    implement Logout, and Dashboard page

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ use DeveloperAnnur\Belajar\PHP\MVC\App\Router;
 use DeveloperAnnur\Belajar\PHP\MVC\Config\Database;
 use DeveloperAnnur\Belajar\PHP\MVC\Controller\HomeController;
 use DeveloperAnnur\Belajar\PHP\MVC\Controller\UserController;
+use DeveloperAnnur\Belajar\PHP\MVC\Middleware\MustLoginMiddleware;
+use DeveloperAnnur\Belajar\PHP\MVC\Middleware\MustNotLoginMiddleware;
 
 Database::getConnection('prod');
 
@@ -13,9 +15,10 @@ Database::getConnection('prod');
 Router::add('GET', '/', HomeController::class, 'index', []);
 
 // User Controller
-Router::add('GET', '/users/register', UserController::class, 'register', []);
-Router::add('POST', '/users/register', UserController::class, 'postRegister', []);
-Router::add('GET', '/users/login', UserController::class, 'login', []);
-Router::add('POST', '/users/login', UserController::class, 'postLogin', []);
+Router::add('GET', '/users/register', UserController::class, 'register', [MustNotLoginMiddleware::class]);
+Router::add('POST', '/users/register', UserController::class, 'postRegister', [MustNotLoginMiddleware::class]);
+Router::add('GET', '/users/login', UserController::class, 'login', [MustNotLoginMiddleware::class]);
+Router::add('POST', '/users/login', UserController::class, 'postLogin', [MustNotLoginMiddleware::class]);
+Router::add('GET', '/users/logout', UserController::class, 'logout', [MustLoginMiddleware::class]);
 
 Router::run();
